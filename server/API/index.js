be948307684db13783a7a2ecba8cea4cacf07c9e
Michael Baez <mrbaez427@gmail.com>
2021-05-29 22:57:43 -0400

    Added authentication, missing redux portion

--- server/API/index.js
+++ server/API/index.js
@@ -6,6 +6,27 @@ const router = require('express').Router();
 
 // Example Route: router.use('/campuses', require('./campuses'));
 
+router.post('/login', async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists');
+    } else {
+      next(err);
+    }
+  }
+});
+
 // API Error Handling
 router.use((req, res, next) => {
   const err = new Error('API route not found!');
